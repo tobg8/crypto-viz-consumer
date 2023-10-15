7514336d8764716e96409bebdbbd069b44bcde82
christian.leng <christian@djob.io>
2023-10-15 18:05:41 +0200

    feat(): init prettier/eslnt/husky && init layout

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routes from './Routes';
+import ThemeConfig from './style/index';
+
+function App() {
+  return (
+    <ThemeConfig>
+      <Routes />
+    </ThemeConfig>
+  );
+}
+
+export default App;
