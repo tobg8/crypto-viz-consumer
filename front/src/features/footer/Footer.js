8e492c31a52fc6a96260ea171f0d6cd41200a76e
christian.leng <christian@djob.io>
2023-10-18 17:24:09 +0200

    wip

---
+++ front/src/features/footer/Footer.js
@@ -0,0 +1,69 @@
+import {
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  ListSubheader,
+  Stack,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <Box component={'footer'} sx={{ py: 4 }}>
+      <Divider />
+      <Stack>
+        <Box>
+          <List
+            subheader={
+              <ListSubheader
+                sx={{ display: 'flex', justifyContent: 'center' }}
+                component="div"
+                id="nested-list-subheader"
+              >
+                Nested List Items
+              </ListSubheader>
+            }
+          >
+            <ListItem sx={{ display: 'flex', flexDirection: 'column' }}>
+              <ListItemText primary="Single-line item" />
+              <ListItemText primary="Single-line item" />
+              <ListItemText primary="Single-line item" />
+              <ListItemText primary="Single-line item" />
+            </ListItem>
+          </List>
+        </Box>
+      </Stack>
+      <Stack
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        flexDirection={'row'}
+        px={2}
+      >
+        <Typography>
+          © {new Date().getFullYear()} CoinMarketCap. All rights reserved
+        </Typography>
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          sx={{
+            mr: 2,
+            display: { xs: 'none', md: 'flex' },
+            fontFamily: 'monospace',
+            fontWeight: 700,
+            letterSpacing: '.3rem',
+            color: 'inherit',
+            textDecoration: 'none',
+          }}
+        >
+          CRYPTO-VIZ
+        </Typography>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Footer;
